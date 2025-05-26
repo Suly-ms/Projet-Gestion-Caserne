a4553a2c7f9268df6bb59bcb4f440fd7b3fdf77b
Josué Martin <josue.martin@unistra.fr>
2025-05-26 23:44:55 +0200

    Partie fonctionnelle complètement terminée. En clôturant, on peut écrire le compte-rendu ainsi que sélectionner les engins endommagés et les réparations à prévoir. Répétition de code parfois -> penser à optimiser ou peut-être à créer un fichier .cs contenant les fonctions de récupération de données pour éviter de les écrire plusieurs fois dans différents Forms

---
+++ SAE A21-D21 - Projet Caserne/CompteRenduBox/CompteRenduBox/FrmCompteRendu.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace CompteRenduBox
+{
+    partial class FrmCompteRenduBox
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbRapport = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbRapport
+            // 
+            this.txbRapport.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.txbRapport.Location = new System.Drawing.Point(17, 36);
+            this.txbRapport.Name = "txbRapport";
+            this.txbRapport.Size = new System.Drawing.Size(387, 24);
+            this.txbRapport.TabIndex = 0;
+            this.txbRapport.TextChanged += new System.EventHandler(this.txbRapport_TextChanged);
+            this.txbRapport.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbRapport_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(80, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(273, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Veuillez écrire le rapport de la mission";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmCompteRenduBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(420, 74);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbRapport);
+            this.Name = "FrmCompteRenduBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Compte rendu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbRapport;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SAE A21-D21 - Projet Caserne/SAE A21-D21 - Projet Caserne/FrmDommageEngin.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SAE_A21_D21___Projet_Caserne
+{
+    partial class FrmDommageEngin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblReparation = new System.Windows.Forms.Label();
+            this.txbReparation = new System.Windows.Forms.TextBox();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblReparation
+            // 
+            this.lblReparation.AutoSize = true;
+            this.lblReparation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReparation.ForeColor = System.Drawing.Color.Black;
+            this.lblReparation.Location = new System.Drawing.Point(9, 9);
+            this.lblReparation.Name = "lblReparation";
+            this.lblReparation.Size = new System.Drawing.Size(397, 20);
+            this.lblReparation.TabIndex = 5;
+            this.lblReparation.Text = "Veuillez specifier les dégâts subis sur l\'engin 0-ABCD-0";
+            this.lblReparation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txbReparation
+            // 
+            this.txbReparation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.txbReparation.Location = new System.Drawing.Point(28, 34);
+            this.txbReparation.Multiline = true;
+            this.txbReparation.Name = "txbReparation";
+            this.txbReparation.Size = new System.Drawing.Size(357, 93);
+            this.txbReparation.TabIndex = 4;
+            // 
+            // btnValider
+            // 
+            this.btnValider.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnValider.Location = new System.Drawing.Point(325, 133);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(88, 28);
+            this.btnValider.TabIndex = 6;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
+            // 
+            // FrmDommageEngin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 170);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.lblReparation);
+            this.Controls.Add(this.txbReparation);
+            this.Name = "FrmDommageEngin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dégâts";
+            this.Load += new System.EventHandler(this.FrmDommageEngin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblReparation;
+        private System.Windows.Forms.TextBox txbReparation;
+        private System.Windows.Forms.Button btnValider;
+    }
+}
